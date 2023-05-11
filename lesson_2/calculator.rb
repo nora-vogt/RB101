f0272f0bdd2fa53095df52686738f05d57770f95
Nora Vogt <noravogt@protonmail.com>
2023-05-11 10:48:52 -0400

    fixed formatting from rubocop

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,7 +1,7 @@
 # ask the user for two numbers
 # ask the user for an operation to perform
 # perform the operation on the two numbers
-# output the result 
+# output the result
 
 # answer = Kernel.gets()
 # Kernel.puts(answer)
@@ -88,15 +88,15 @@ loop do # main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-          when '1'
-            number1.to_i() + number2.to_i()
-          when '2'
-            number1.to_i() - number2.to_i()
-          when '3'
-            number1.to_i() * number2.to_i()
-          when '4'
-            number1.to_f() / number2.to_f()
-  end
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
