6001e3cecd321b5ebf2a35aafc5f4144fc8bc329
Nora Vogt <noravogt@protonmail.com>
2023-05-15 17:44:34 -0400

    fixed multi-language, removed use of constant

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -7,14 +7,13 @@ require 'pry'
 
 # using a constant - prompts are not expected to change
 MESSAGES = YAML.load_file('calculator_messages.yml')
-LANGUAGE = 'en'
 
-def return_msg(key) # return the value of message from YAML file
-  MESSAGES[LANGUAGE][key]
+def message(key, language='en')
+  MESSAGES[language][key]
 end
 
-def print_msg(key) # print messages from YAML file
-  Kernel.puts("#{return_msg(key)}")
+def print_msg(key, language='en') 
+  puts "=> #{message(key, language)}"
 end
 
 
@@ -30,77 +29,80 @@ def number?(num)
   integer?(num) || float?(num)
 end
 
-def operation_to_message(op)
+def operation_to_message(op, language)
   message = case op
             when '1'
-              return_msg(:adding)
+              message(:adding, language)
             when '2'
-              return_msg(:subtracting)
+              message(:subtracting, language)
             when '3'
-              return_msg(:multiplying)
+              message(:multiplying, language)
             when '4'
-              return_msg(:dividing)
+              message(:dividing, language)
             end
   message
 end
 
 print_msg(:welcome)
+#print_msg(:welcome, 'es')
+
 
 print_msg(:choose_lang)
+#print_msg(:choose_lang, 'es')
 
-language = ''
+language = 'en'
 loop do
-  language = Kernel.gets().chomp()
+  lang_choice = Kernel.gets().chomp()
 
-  if %[1 2].include?(language)
-    LANGUAGE = 'es' if language == '2'
+  if %[1 2].include?(lang_choice)
+    language = 'es' if lang_choice == '2'
     break
   else
-    print_msg(:invalid_lang)
+    print_msg(:invalid_lang, language)
   end
 end
 
-print_msg(:enter_name)
+print_msg(:enter_name, language)
 user_name = ''
 loop do
   user_name = Kernel.gets().chomp()
 
   if user_name.empty?()
-    print_msg(:need_valid_name)
+    print_msg(:need_valid_name, language)
   else
     break
   end
 end
 
-# format(return_msg(key_from_yaml), var_name_from_yaml: local_var)
-puts format(return_msg(:greeting), name: user_name)
+# format(message(key_from_yaml), var_name_from_yaml: local_var)
+puts format(message(:greeting, language), name: user_name)
 
 loop do # main loop
   number1 = ''
   loop do
-    print_msg(:first_num)
+    print_msg(:first_num, language)
     number1 = Kernel.gets().chomp()
 
     if number?(number1)
       break
     else
-      print_msg(:invalid_num)
+      print_msg(:invalid_num, language)
     end
   end
 
   number2 = ''
   loop do
-    print_msg(:second_num)
+    print_msg(:second_num, language)
     number2 = Kernel.gets().chomp()
 
     if number?(number2)
       break
     else
-      print_msg(:invalid_num)
+      print_msg(:invalid_num, language)
     end
   end
 
-  print_msg(:choose_operation)
+  print_msg(:choose_operation, language)
 
   operator = ''
   loop do
@@ -109,11 +111,13 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      print_msg(:invalid_operator)
+      print_msg(:invalid_operator, language)
     end
   end
 
-  puts format(return_msg(:display_operation), op_name_as_verb: operation_to_message(operator))
+  puts format(message(:display_operation, language), op_name_as_verb: operation_to_message(operator, language))
+
+  sleep(1)
 
   result = case operator
            when '1'
@@ -130,11 +134,11 @@ loop do # main loop
             end
            end
 
-  puts format(return_msg(:result), answer: result)
+  puts format(message(:result, language), answer: result)
 
-  print_msg(:again?)
+  print_msg(:again?, language)
   answer = Kernel.gets().chomp()
   break unless answer.downcase == 'y'
 end
 
-print_msg(:goodbye)
+print_msg(:goodbye, language)
