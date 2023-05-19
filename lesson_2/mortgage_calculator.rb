1c0514be3ada61cd4ed166e63bc2ce907f4c19aa
Nora Vogt <noravogt@protonmail.com>
2023-05-19 12:04:01 -0400

    moved prompts, added a screen clear

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,4 +1,6 @@
-require 'pry'
+def prompt(message)
+  puts "=> #{message}"
+end
 
 =begin
 Regex for loan amount & interest rate validation:
@@ -9,10 +11,6 @@ Regex for loan amount & interest rate validation:
 $      at the end of the string
 (maximum of 2 decimal places is allowed)
 =end
-
-def prompt(message)
-  puts "=> #{message}"
-end
 
 def valid_number?(string)
   /^\d+\.?\d?\d?$/.match(string) && string.to_f > 0
@@ -29,13 +27,17 @@ def read_duration(unit_of_time)
 
     return duration if valid_int?(duration)
 
-    prompt("Invalid entry. Enter the number of #{unit_of_time} as a positive number.") 
+    prompt("Invalid entry. Enter the #{unit_of_time} as a positive number.")
     prompt("(Example: 18)")
   end
 end
 
+system("clear")
 puts "\n-------------------------------------"
-prompt("Welcome to Loan Calculator!")
+prompt("Welcome to Mortgage Calculator!")
+puts <<-INTRO
+  You can use this calculator to calculate your monthly payment.
+INTRO
 puts "-------------------------------------"
 puts "\n"
 
@@ -47,27 +49,29 @@ loop do
 
     break if valid_number?(loan_amount)
 
-    prompt("Invalid amount. Enter your loan amount as a positive number.") 
+    prompt("Invalid amount. Enter your loan amount as a positive number.")
     prompt("(Example: 100000 or 5000.50)")
   end
 
-  annual_percentage_rate = ''
   puts "\n"
   prompt("Please enter your interest rate:")
   prompt("(Example: Enter 5 for 5% or 3.25 for 3.25%)")
+
+  annual_percentage_rate = ''
   loop do
     annual_percentage_rate = gets.chomp
 
     break if valid_number?(annual_percentage_rate)
 
-    prompt("Invalid interest rate. Enter your interest rate as a positive number.")
+    prompt("Invalid entry. Enter the interest rate as a positive number.")
   end
 
-  term_in_months = ''
-  term_in_years = ''
   puts "\n"
   prompt("How would you like to enter your loan term?")
   prompt("(Enter 'months' or 'years')")
+
+  term_in_months = ''
+  term_in_years = ''
   loop do
     term = gets.chomp.downcase
 
@@ -94,12 +98,12 @@ loop do
 
   monthly_int_rate = annual_percentage_rate.to_f / (12 * 100)
 
-  monthly_payment = loan_amount.to_f * 
-                    (monthly_int_rate / 
+  monthly_payment = loan_amount.to_f *
+                    (monthly_int_rate /
                     (1 - (1 + monthly_int_rate)**(-term_in_months)))
 
   puts "\n"
-  prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}")  
+  prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}")
 
   puts "\n"
   prompt("Would you like to calculate another payment? (Y to calculate again):")
