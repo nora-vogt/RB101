9d07703fa52e826ac06b15624020f98a616201ce
Nora Vogt <noravogt@protonmail.com>
2023-05-24 15:30:01 -0400

    extracted rules to method

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,24 +1,19 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
-def test_method
-  prompt("test method")
-end
-
-# test_method # will raise an error; can't invoke a method before it has been defined
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-# test_method # works here; methods can be defined in any order
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
 
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
