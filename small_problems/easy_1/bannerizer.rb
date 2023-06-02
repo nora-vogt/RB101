b9a2b8055851ab10921165bcac3b115d36a0004a
Nora Vogt <noravogt@protonmail.com>
2023-06-02 15:27:51 -0400

    added further exploration truncate method

--- small_problems/easy_1/bannerizer.rb
+++ small_problems/easy_1/bannerizer.rb
@@ -9,6 +9,21 @@ def print_in_box(string)
   puts horizontal_line
 end
 
-print_in_box('To boldly go where no one has gone before.')
+def print_and_truncate_in_box(string)
+  string = string[0..75] if string.length > 76  
+  horizontal_line = "+#{'-' * (string.length + 2)}+"
+  spacer_line = "|#{' ' * (string.length + 2)}|"
 
+  puts horizontal_line
+  puts spacer_line
+  puts "| #{string} |"
+  puts spacer_line
+  puts horizontal_line
+end
+
+
+
+print_in_box('To boldly go where no one has gone before.')
 print_in_box('')
+
+print_and_truncate_in_box("This is a really, really long string that is going to go off the screen, that's how long this string is")
