5f5d5e516fc515a9919792f25cbc286f9a4f2e88
Nora Vogt <noravogt@protonmail.com>
2023-06-08 11:20:34 -0400

    completed further exploration

--- small_problems/easy_2/tip_calculator.rb
+++ small_problems/easy_2/tip_calculator.rb
@@ -5,8 +5,8 @@ print "What is the tip percentage? "
 tip_percentage = gets.to_f
 
 tip_rate = tip_percentage / 100
-tip_amount = (bill * tip_rate).round(2)
-total_bill = (bill + tip_amount).round(2)
+tip_amount = bill * tip_rate
+total_bill = bill + tip_amount
 
-puts "The tip is $#{tip_amount}"
-puts "The total is $#{total_bill}"
+puts "The tip is $#{format('%.2f', tip_amount)}"
+puts "The total is $#{format('%.2f', total_bill)}"
