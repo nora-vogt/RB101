e3c5cdfa8cf00e31cca145eb6d2fd61ee607852c
Nora Vogt <noravogt@protonmail.com>
2023-06-09 16:11:21 -0400

    extracted loops to helper methods

--- small_problems/sum_or_product.rb
+++ small_problems/sum_or_product.rb
@@ -6,34 +6,50 @@ def valid_choice?(choice)
   choice == 's' || choice == 'p'
 end
 
-loop do
+def get_number
   puts ">> Please enter an integer greater than 0:"
-
-  number = nil
   loop do
     number = gets.to_i
 
-    break if valid_integer?(number)
+    return number if valid_integer?(number)
+
     puts "Invalid entry. Enter a whole number greater than 0:"
   end
+end
 
+def get_choice
   puts ">> Enter 's' to compute the sum, 'p' to compute the product:"
-  product = nil
-  calculation_choice = ''
   loop do
-    calculation_choice = gets.chomp.downcase
+    choice = gets.chomp.downcase
 
-    break if valid_choice?(calculation_choice)
+    return choice if valid_choice?(choice)
+
     puts "Invalid choice. Enter 's' for sum or 'p' for product:"
   end
 
+end
+
+def calculate_sum(number)
+  sum = 0
+  (1..number).each { |n| sum += n }
+  sum
+end
+
+def calculate_product(number)
+  product = 1
+  (1..number).each { |n| product *= n }
+  product
+end
+
+loop do
+  number = get_number
+  calculation_choice = get_choice
+  
   if calculation_choice == 's'
-    sum = 0
-    (1..number).each { |n| sum += n }
+    sum = calculate_sum(number)
     puts "The sum of integers between 1 and #{number} is #{sum}."
   elsif calculation_choice == 'p'
-    product = 1
-    (1..number).each { |n| product *= n }
+    product = calculate_product(number)
     puts "The product of integers between 1 and #{number} is #{product}."
   end
 end
