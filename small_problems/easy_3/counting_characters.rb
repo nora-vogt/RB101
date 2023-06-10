7b360de36e9a82690c433a9a2837f2ced987f376
Nora Vogt <noravogt@protonmail.com>
2023-06-10 11:06:20 -0400

    refactored solution

--- small_problems/easy_3/counting_characters.rb
+++ small_problems/easy_3/counting_characters.rb
@@ -1,7 +1,7 @@
 print "Please write a word or multiple words: "
 string = gets.chomp
+number_of_characters = string.gsub(' ', '').size
 
-string_with_no_spaces = string.gsub(' ', '')
-puts "There are #{string_with_no_spaces.size} characters in \"#{string}\"."
+puts "There are #{number_of_characters} characters in \"#{string}\"."
 
 # From solution, could also use String#delete to remove spaces
